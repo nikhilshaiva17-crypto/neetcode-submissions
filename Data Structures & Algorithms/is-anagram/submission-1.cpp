cdd4c8525bec449ffdbad7503f6e4c79bad05655
Nikhil-codes <nikhilshaiva17@gmail.com>
2026-07-14 22:37:05 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      int ump[26]={0};
+      for(char i:s){
+        ump[i-'a']++;
+      }
+      for(char j:t){ ump[j-'a']--; }
+
+      for(auto k :ump){
+        if(k!=0)return false;
+      }
+       return true;
+    }
+};
